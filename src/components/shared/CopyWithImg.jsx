04e956277d78090869fc3201e35df1fe38a34287
Chris <chris79gp@outlook.com>
2024-08-30 17:38:04 +0100

    Feat: add `emphasis` prop to `CopyWithImg`

--- src/components/shared/CopyWithImg.jsx
+++ src/components/shared/CopyWithImg.jsx
@@ -1,12 +1,22 @@
 import PropTypes from "prop-types";
 import "../../styles/shared/CopyWithBento.css";
 
-export default function CopyWithBento({ children, imgSrc, imgAltText, reverse = false }) {
+export default function CopyWithBento({
+	children,
+	imgSrc,
+	imgAltText,
+	reverse = false,
+	emphasis = false,
+}) {
 	const copy = <div className="copy">{children}</div>;
 	const img = <img src={imgSrc} alt={imgAltText} />;
 
 	return (
-		<div className={`col-content-container ${reverse ? "reverse" : ""}`}>
+		<div
+			className={`col-content-container copy-img ${emphasis ? "emphasis" : ""} ${
+				reverse ? "reverse" : ""
+			}`}
+		>
 			{reverse ? (
 				<>
 					{img}
@@ -27,4 +37,5 @@ CopyWithBento.propTypes = {
 	imgSrc: PropTypes.string.isRequired,
 	imgAltText: PropTypes.string,
 	reverse: PropTypes.bool,
+	emphasis: PropTypes.bool,
 };
