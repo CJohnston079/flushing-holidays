d09942671a9e0a73497cd0ad5f7d2ce7eddf1c4c
Chris <chris79gp@outlook.com>
2024-08-31 10:18:39 +0100

    Feat: remove "Leave a testimonial" button from `Testimonials.jsx`

--- src/components/sections/Testimonials.jsx
+++ src/components/sections/Testimonials.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
 import Button from "../shared/Button";
 import FlexContainer from "../shared/FlexContainer";
-import Modal from "../shared/Modal";
-import ModalForm from "../shared/ModalForm";
 import Section from "../shared/Section";
 import Testimonial from "../shared/Testimonial";
-import formFields from "../../data/formFields.json";
 import reviews from "../../data/reviews.json";
 import "../../styles/sections/Testimonials.css";
 
@@ -40,13 +37,8 @@ export default function Testimonials() {
 	return (
 		<Section sectionId="testimonials">
 			<FlexContainer>{testimonials}</FlexContainer>
-			<div className="t-buttons">
+			<div className="t-buttons" style={{ justifyContent: "center" }}>
 				<Button onClick={handleShowMoreTestimonials}>Show more testimonials</Button>
-				<Modal buttonText="Leave a testimonial">
-					{({ closeModal }) => (
-						<ModalForm fieldsData={formFields.testimonials} handleClose={closeModal} />
-					)}
-				</Modal>
 			</div>
 		</Section>
 	);
